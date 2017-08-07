9dd00c9aed7597d7e145b92d42c2f62b07c875bb
Bhaskar, Anil <anil.bhaskar@rakuten.com>
2017-08-07 09:36:52 +0530

    Commenting out the security code to prevent default spring security realm being applied

--- src/main/java/com/mytaxi/security/SecurityConfig.java
+++ src/main/java/com/mytaxi/security/SecurityConfig.java
@@ -4,12 +4,12 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig {/*extends WebSecurityConfigurerAdapter{
 	
 	//@EnableWebSecurity
 	public static class SampleMultiHttpSecurityConfig {
@@ -22,10 +22,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		  .usersByUsernameQuery(
 		   "SELECT username, password FROM driver WHERE username=?")
 		  .authoritiesByUsernameQuery(
-		  "SELECT useremail, role FROM tm_admin WHERE useremail=?");
+		  "SELECT useremail, role FROM driver WHERE username=?");
 		 } 
 	
-	@Configuration
+	//@Configuration
 	public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
 		protected void configure(HttpSecurity http) throws Exception {
 			http				
@@ -44,4 +44,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	    	}
 	}
 
-}}
+}*/}
